c127b9bad1e25f5562ae2037ae71196990eaed5b
analuscharf <analuscharf@gmail.com>
2016-05-14 09:23:05 -0300

    Update versa1banco.sql
    Realizando teste do banco de dados.Tem que terminar a tarefa de cadastramento.

--- versa1banco.sql
+++ versa1banco.sql
@@ -1,114 +1,82 @@
+#Teste.1 das tabelas do banco de dados.
 use db1220018384;
 
-#Remove todas as tabelas tem esse nome.
-drop table if exists processoDeMatricula;
-drop table if exists matricula;
-drop table if exists horario;
-drop table if exists preRequisito;
-drop table if exists disciplina;
-drop table if exists professor;
-drop table if exists curso;
-drop table if exists aluno; 
-
-
-#aluno(*cpf,nome)
-create table aluno(
-	cpf   bigint unsigned primary key,
-    nome  varchar(50),
-    senha varchar(50) 
-);
 desc aluno;
+insert  into aluno(cpf,nome)
+values
+      ('1','Ana'),
+	  ('2','Leonan'),
+	  ('3','karoline'),
+	  ('4','Beatriz'),
+	  ('5','Murilo');
+select * from aluno;
+select * from aluno where cpf='1'; #Retorna as linhas, que possui esse valor. 
+select nome from aluno where cpf='1'; #Retorna só o nome.
 
-#professor(*siape,nome);
-create table professor(
-     siape  int unsigned primary key, 
-     nome   varchar (50)
-);
+
 desc professor;
+insert into professor(siape,nome)
+values
+	  ('1','Ederson'),
+	  ('2','Volnei'),
+	  ('3','Arliones'),
+	  ('4','Bruno'),
+	  ('5','Noronha');
+select *  from professor where nome regexp 'E'; #Retorna todos os nomes com a letra 'e'. Não é sensitive case. Se o argumento  for [EA], retorna   quem possui 'e' ou 'a'
+select *  from professor where nome like 'e%';  #Retorna  que começa com a letra 'e'. Se for '%e', retorrna quem termina com o 'e';E "%a%" retorna quem começa e termina com com o caracter.
 
-#curso(*codigo,nome);
-create table curso(
-    codigo  smallint unsigned  primary key,
-    nome    varchar(50)
-);
-desc curso;
 
-#Disciplina (*cursoCodigo, fase, carga horária, **grade, carga horária mínima, **professorSiape)
-#cursoCodigo referência curso(código) 
-#professorSiape referência professor(siape) 
-create table disciplina(
-    codigo         char(4) primary key,
-    fase           tinyint unsigned,
-	CH             smallint unsigned,
-    CHminima       smallint unsigned,
-    cursoCodigo    smallint unsigned,
-    professorSiape int unsigned,  
-    constraint fk_disciplina_cursoCodigo_curso_codigo       
-			   foreign key (cursoCodigo) references curso(codigo), 
-    constraint fk_disciplina_professorSiape_professor_siape 
-			   foreign key (professorSiape) references professor(siape) 
-);
+desc  curso;
+
+insert into curso(codigo,nome) 
+values 
+	  ('299','Engenharia de Telecomunicaçções'); # Inserir nome errado.
+
+update curso set nome = 'Engenharia de Telecomunicações'  where codigo = '299'; #Arrumando o nome da disciplina.
+update curso set codigo= '290'  where codigo = '299'; #Arrumando o nome da disciplina.
+
+select * from curso;
+
+
 desc disciplina;
+insert into disciplina(codigo, fase, CH, CHminima, cursoCodigo,professorSiape)
+	values ('BCD','8','54',NULL,'290','1');
+select * from disciplina;
 
-#Pré-requisito (***disciplina,***pré-requisito)
-#disciplina referência disciplina(código)
-#pré-requisito referência disciplina(código)
-create table preRequisito (
-	id 		     mediumint unsigned auto_increment primary key,
-    disciplina   char(4),
-    preRequisito char(4),
-    constraint fk_preRequisito_disciplina_disciplina_codigo   
-			   foreign key (disciplina)   references disciplina(codigo), 
-    constraint fk_preRequisito_preRequisito_disciplina_codigo  
-			    foreign key (preRequisito) references disciplina(codigo)
+insert into disciplina(codigo, fase, CH, CHminima, cursoCodigo,professorSiape)
+	values ('BCD2','8','54',NULL,'290','1');
+select * from disciplina;
+
+
+select siape from  professor where nome='Arliones'; 
+#Procurando e adicionamos ao mesmo tempo.
+insert into disciplina(codigo, fase, CH, CHminima, cursoCodigo,professorSiape)
+	values ('STE','8','72',NULL,'290',(select siape from  professor where nome='Arliones')
+   );
+select * from disciplina;
+
+#Procurando em quais disciplina que o professro fulano ministra.
+select *
+from disciplina
+where professorSiape =(select siape from professor where nome ='Ederson')
+
+select * from professor;
+select siape from professor where nome ='Ederson';
+insert into disciplina(codigo, fase, CH, CHminima, cursoCodigo,professorSiape)
+	values ('PJI3','8','36',NULL,
+           (select codigo from curso where nome ='Engenharia de Telecomunicações'),
+		   (select siape from professor where  nome = 'Noronha')
 );
-desc preRequisito;
+select * from disciplina;
 
-#Horário (*turno, *num_aula, *num_dia, *semanaAB, ***disciplina, semestre)
-#Obs: não pode ter o chave estrangeira com nome repetido.
-#disciplina referência disciplina(código)
-create table horario (
-   id 					tinyint unsigned auto_increment primary key,
-   numAula              tinyint unsigned,  
-   numDia				tinyint unsigned, 
-   semanaAB				tinyint unsigned,
-   codigo				tinyint unsigned,
-   codigoDisciplina		char(4),
-   semestre             smallint unsigned,
-   constraint fk_horario_codigoDisciplina_disciplina_codigo  
-             foreign key (codigoDisciplina)   references disciplina(codigo) 
-);
-desc horario;
 
-#matrícula(***aluno, ***curso, matrícula)
-#aluno referência aluno(cpf)
-#curso referência curso(codigo) 
-create table  matricula(
-	id			smallint unsigned auto_increment primary key,
-	aluno       bigint unsigned,
-	curso       smallint unsigned,
-	matricula   int unsigned,
-    constraint fk_matricula_aluno_aluno_codigo 
-			 foreign key  (aluno) references aluno(cpf),
-	constraint fk_matricula_curso_curso_codigo 
-			 foreign key  (curso) references curso(codigo)
+# Tarefa
+# Realiza todo processo de matricula.
 
- );
-desc matricula;
+#Passo 1: update dos meus dados.
+update aluno set nome = 'Ana Luiza Scharf' where cpf = '1';
+update aluno set cpf = '0676617705' where cpf = '1';
+select * from aluno order by  cpf desc;
 
-#Processo_de_Matrícula (***matricula, ***disciplina, concluído, semestre)
-#matricula referência matricula(aluno,curso)
-#disciplina referência disciplina(codigo)
-create table processoDeMatricula (
-	id		    smallint unsigned auto_increment primary key,
-	matricula   smallint unsigned,
-	disciplina  char(4),
-	concluido   tinyint unsigned,
-	semestre    tinyint unsigned,
-    constraint fk_processoDeMatricula_matricula_matricula_id 
-			   foreign key  (matricula) references matricula(id),
-    constraint fk_processoDeMatricula_disciplina_disciplina_codigo 
-			   foreign key  (disciplina) references disciplina(codigo)
-      
-);
-desc processoDeMatricula;
+#Passo2: adicinar os professor da 8 fase.
+
